c43b85603512fd521991c60523774b70e4224f90
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-11-26 23:02:21 +0530

    updated fe live url

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(
   require("cors")({
     origin: [
       "http://localhost:5173", // dev frontend (vite)
-      "https://www.ictfaculty.live/"
+      "https://www.ictfaculty.live"
     ],
     credentials: true,
   })
